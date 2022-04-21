94dc583fb78aa8024c748680f2ec02621e78490c
Utkarsh Chirimar <utkarsh.chirimar@gmail.com>
2022-04-21 15:48:24 -0400

    publish april tag visualization

--- catkin_ws/src/lab_code/scripts/april_tag.py
+++ catkin_ws/src/lab_code/scripts/april_tag.py
@@ -9,26 +9,65 @@ import apriltag
 
 detector = apriltag.Detector()
 bridge = CvBridge()
+result = []
+image = None
+val = False
+threshold = 1
 
 
 def save_image(data):
-	global bridge, detector
-	print('Received Image')
+	global bridge, detector, result, image, val
 	try:
-		cv2_img = bridge.imgmsg_to_cv2(data, "mono8")
+		image = bridge.imgmsg_to_cv2(data, "rgb8")
+		gray = bridge.imgmsg_to_cv2(data, "mono8")
 	except CvBridgeError, e:
 		print(e)
 		return
 
-	result = detector.detect(cv2_img)
+	result = detector.detect(gray)[0]
+	val = True
 	print(result)
 
 
 def main():
+	global result, image, bridge, val
 	rospy.init_node('april_tag', anonymous=True)
 	rospy.Subscriber('/head_camera/rgb/image_raw', Image, save_image)
+	image_pub = rospy.Publisher('/april_tag', Image, queue_size=1)
 
-	rospy.spin()
+	rate = rospy.Rate(10)
+
+	while not rospy.is_shutdown():
+		if val:
+			# extract the bounding box (x, y)-coordinates for the AprilTag
+			# and convert each of the (x, y)-coordinate pairs to integers
+			(ptA, ptB, ptC, ptD) = result.corners
+			ptB = (int(ptB[0]), int(ptB[1]))
+			ptC = (int(ptC[0]), int(ptC[1]))
+			ptD = (int(ptD[0]), int(ptD[1]))
+			ptA = (int(ptA[0]), int(ptA[1]))
+			# draw the bounding box of the AprilTag detection
+			cv2.line(image, ptA, ptB, (0, 255, 0), 2)
+			cv2.line(image, ptB, ptC, (0, 255, 0), 2)
+			cv2.line(image, ptC, ptD, (0, 255, 0), 2)
+			cv2.line(image, ptD, ptA, (0, 255, 0), 2)
+			# draw the center (x, y)-coordinates of the AprilTag
+			(cX, cY) = (int(result.center[0]), int(result.center[1]))
+			cv2.circle(image, (cX, cY), 5, (0, 0, 255), -1)
+			# draw the tag family on the image
+			tagFamily = result.tag_family.decode("utf-8")
+			cv2.putText(image, tagFamily, (ptA[0], ptA[1] - 15),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+			
+			image_pub.publish(bridge.cv2_to_imgmsg(image))
+
+			print(image.shape)
+			print(cY)
+
+			image_cY = image.shape[0] // 2
+			print(cY == image_cY)
+
+		rate.sleep()
 
 
 if __name__ == '__main__':
